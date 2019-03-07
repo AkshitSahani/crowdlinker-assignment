0a63721b417a418bcb36d044150ed1316c761957
Akshit Sahani <akshit_sahani@hotmail.com>
2019-03-07 17:42:31 -0500

    updated seeds and formatting

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-titles = ['Kelbessa Negewo', 'State of Conflict', "A Convict's Odyssey" ,
-          'Hitting Bottom', 'I Want This Apartment',]
+titles = [
+  'Kelbessa Negewo',
+  'State of Conflict',
+  "A Convict's Odyssey" ,
+  'Hitting Bottom',
+  'I Want This Apartment',
+  'Old tunnels found under Alcatraz prison'
+]
 
-descriptions = ['In the 1970s, Kelbessa Negewo was a midlevel administrator'\
-                'in Ethiopia’s brutal Red Terror regime. In the 1990s'\
-                'he was a bellhop in an Atlanta hotel.'\
-                'Then someone he had tortured back home recognized him.',
-                'How a tiny protest at the University of Nebraska turned'\
-                'into a proxy war for the future of campus politics.',
-                'When he was 16, Mark Clements talked his way into four'\
-                'life sentences. Twenty-eight years later, he talked his'\
-                'way out.',
-                'Is Dr. Drew’s “Celebrity Rehab” therapy or tabloid voyeurism?',
-                'War stories from the world of Manhattan real estate, written'\
-                'during an era when everybody knew the Internet would completely'\
-                'change the business and nobody quite knew how.']
-titles.each_with_index { |title, index| Article.create(title: titles[index], description: [descriptions[index]])  }
+descriptions = [
+  'In the 1970s, Kelbessa Negewo was a midlevel administrator in Ethiopia’s brutal Red Terror regime. In the 1990s he was a bellhop in an Atlanta hotel, until someone he had tortured back home recognized him.',
+  'How a tiny protest at the University of Nebraska turned into a proxy war for the future of campus politics.',
+  'When he was 16, Mark Clements talked his way into four life sentences. Twenty-eight years later, he talked his way out.',
+  'Is Dr. Drew’s “Celebrity Rehab” therapy or tabloid voyeurism?',
+  'War stories from the world of Manhattan real estate, written during an era when everybody knew the Internet would completely change the business and nobody quite knew how.'
+  "Civil War-era fortifications have been discovered just below the prison's recreation yard, a study shows"
+]
+
+titles.each_with_index { |title, index| Article.create(title: title, description: descriptions[index])  }
